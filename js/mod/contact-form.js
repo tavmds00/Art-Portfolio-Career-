f1d01724d2fcb8aa158592af1952618528f884ae
mas <tomasvmoreno@gmail.com>
2026-08-26 13:53:14 -0500

    Fix success message alignment

--- js/mod/contact-form.js
+++ js/mod/contact-form.js
@@ -72,7 +72,7 @@
                         $('#contact-title').text('SUCCESS');
 
                        form.html(
-								'<p class="success" style="text-align:left;!important;">Your message has been sent.</p>'
+								'<p class="success" style="text-align:left;!important;">Your message has been sent!</p>'
 								);
 
                     }else{
